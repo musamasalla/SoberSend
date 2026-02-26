c52bea8d34e6482c6ec1446a108ffbc33172e0c2
try-hard1010 <136584339+musamasalla@users.noreply.github.com>
2026-02-26 07:32:53 +0200

    Initial Commit

---
+++ SoberSend/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SoberSend
+//
+//  Created by Musa Masalla on 2026/02/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
